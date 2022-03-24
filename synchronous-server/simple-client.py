976f5623a78a0d1400afad5dc476b92b63cc503f
Manan Patel <pate0658@algonquinlive.com>
2022-03-24 18:06:50 -0400

    Organized files into folders for readability

---
+++ synchronous-server/simple-client.py
@@ -0,0 +1,26 @@
+import socket
+
+HOST = '127.0.0.1'
+PORT = 8888
+
+
+class Client:
+    """
+    Implementation of a simple client using TCP socket programming that
+    sends a simple message to the standard loop-back interface address.
+    Phases of HTTP message exchange over TCP by the client:
+        Client: socket -> connect -> send -> recv -> close
+    """
+
+    def __init__(self):
+        # AF_INET - IPv4 address family, SOCK_STREAM - TCP (connection-based)
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+            client_socket.connect((HOST, PORT))
+            request_message = b"Hello!"
+            client_socket.sendall(request_message)
+            response = client_socket.recv(1024)
+            print(response)
+
+
+if __name__ == "__main__":
+    Client()
